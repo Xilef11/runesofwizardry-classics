3820e956da10a26ab486328e1e563a0e49caf166
Xilef11 <Xilef11@users.noreply.github.com>
2016-03-05 17:53:28 -0500

    Updated patterns and lang to reflect registering changes in the core

--- src/main/java/xilef11/mc/runesofwizardry_classics/client/gui/ModGuiConfig.java
+++ src/main/java/xilef11/mc/runesofwizardry_classics/client/gui/ModGuiConfig.java
@@ -5,7 +5,6 @@ package xilef11.mc.runesofwizardry_classics.client.gui;
 
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.common.config.ConfigElement;
-import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.config.GuiConfig;
 import xilef11.mc.runesofwizardry_classics.Config;
 import xilef11.mc.runesofwizardry_classics.Refs;
