dd6674f04e75b5519d96a3aefdb801d942b2c9d4
Xilef11 <Xilef11@users.noreply.github.com>
2016-03-17 15:49:55 -0400

    implemented rune of bouncing

--- src/main/java/xilef11/mc/runesofwizardry_classics/runes/RuneBouncing.java
+++ src/main/java/xilef11/mc/runesofwizardry_classics/runes/RuneBouncing.java
@@ -4,20 +4,31 @@ package xilef11.mc.runesofwizardry_classics.runes;
 import java.io.IOException;
 import java.util.Set;
 
-import xilef11.mc.runesofwizardry_classics.Refs;
-import xilef11.mc.runesofwizardry_classics.runes.entity.RuneEntityUnimplemented;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.Vec3i;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import xilef11.mc.runesofwizardry_classics.Refs;
+import xilef11.mc.runesofwizardry_classics.runes.entity.RuneEntityBouncing;
 
 import com.zpig333.runesofwizardry.api.RuneEntity;
+import com.zpig333.runesofwizardry.core.WizardryRegistry;
 import com.zpig333.runesofwizardry.core.rune.PatternUtils;
 import com.zpig333.runesofwizardry.tileentity.TileEntityDustActive;
+import com.zpig333.runesofwizardry.tileentity.TileEntityDustPlaced;
 
 public class RuneBouncing extends ClassicRune {
-
+	public RuneBouncing(){
+		MinecraftForge.EVENT_BUS.register(this);
+	}
 	@Override
 	protected ItemStack[][] setupPattern() throws IOException {
 		return PatternUtils.importFromJson(Refs.PATTERN_PATH+"runeBouncing.json");
@@ -43,9 +54,30 @@ public class RuneBouncing extends ClassicRune {
 	@Override
 	public RuneEntity createRune(ItemStack[][] actualPattern, EnumFacing front,
 			Set<BlockPos> dusts, TileEntityDustActive entity) {
-		return new RuneEntityUnimplemented(actualPattern, front, dusts, entity, this);
+		return new RuneEntityBouncing(actualPattern, front, dusts, entity);
 	}
-
+	//event handling is done here because this is a singleton, not the rune entity
+	@SubscribeEvent
+	public void onEntityJump(LivingJumpEvent event){
+		EntityLivingBase ent = event.entityLiving;
+		World world = ent.worldObj;
+		if(!world.isRemote){
+			//basically, check if we are standing on placed dust, then check if its an active rune of bouncing
+			BlockPos feetPos = ent.getPosition();
+			IBlockState state = world.getBlockState(feetPos);
+			if(state.getBlock()==WizardryRegistry.dust_placed){
+				TileEntity te = world.getTileEntity(feetPos);
+				if(te instanceof TileEntityDustPlaced){
+					TileEntityDustPlaced ted = (TileEntityDustPlaced)te;
+					RuneEntity rune = ted.getRune();
+					if(rune instanceof RuneEntityBouncing){
+						ent.addVelocity(0, 1D, 0);
+						ent.velocityChanged=true;
+					}
+				}
+			}
+		}
+	}
 }
 
     
---
+++ src/main/java/xilef11/mc/runesofwizardry_classics/runes/entity/RuneEntityBouncing.java
@@ -0,0 +1,59 @@
+package xilef11.mc.runesofwizardry_classics.runes.entity;
+
+import java.util.List;
+import java.util.Set;
+
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
+
+import com.zpig333.runesofwizardry.api.RuneEntity;
+import com.zpig333.runesofwizardry.tileentity.TileEntityDustActive;
+
+public class RuneEntityBouncing extends RuneEntity {
+
+	public RuneEntityBouncing(ItemStack[][] actualPattern, EnumFacing facing,
+			Set<BlockPos> dusts, TileEntityDustActive entity) {
+		super(actualPattern, facing, dusts, entity);
+	}
+
+	@Override
+	public String getRuneID() {
+		return "runeBouncing";
+	}
+
+	@Override
+	public void onRuneActivatedbyPlayer(EntityPlayer player,
+			ItemStack[] sacrifice, boolean negated) {
+		// TODO FX
+
+	}
+
+	@Override
+	public void update() {
+		World world = entity.getWorld();
+		if(!world.isRemote){
+			//get all living on the rune
+//			List<EntityLiving> onRune = world.getEntitiesWithinAABB(EntityLiving.class, new AxisAlignedBB(getPos(), getPos().add(1,1,1)));
+//			for(EntityLiving ent:onRune){
+//				if(ent.motionY>0&&ent.isAirBorne){//if entity is going up (there is probably a better way to see if jumping)
+//					ent.getJumpHelper().setJumping();
+//					ent.getJumpHelper().doJump();
+//					ent.addVelocity(0, 1.27D, 0);
+//					ent.velocityChanged=true;
+//				}
+//			}
+			//remove fall damage for all entities within the action radius
+			int radius=6;
+			List<EntityLiving> negateFall = world.getEntitiesWithinAABB(EntityLiving.class, new AxisAlignedBB(getPos().add(-radius, 0, -radius), getPos().add(radius,radius,radius)));
+			for(EntityLiving ent:negateFall){
+				ent.fallDistance=0;
+			}
+		}
+	}
+
+}
