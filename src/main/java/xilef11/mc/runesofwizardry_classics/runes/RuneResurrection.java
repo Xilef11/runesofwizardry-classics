686920136b1835b7c366acf3314ee582406afcbf
Xilef11 <Xilef11@users.noreply.github.com>
2016-04-07 15:12:36 -0400

    added rune of resurrection

--- src/main/java/xilef11/mc/runesofwizardry_classics/runes/RuneResurrection.java
+++ src/main/java/xilef11/mc/runesofwizardry_classics/runes/RuneResurrection.java
@@ -2,22 +2,50 @@
 package xilef11.mc.runesofwizardry_classics.runes;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
-import xilef11.mc.runesofwizardry_classics.Refs;
-import xilef11.mc.runesofwizardry_classics.runes.entity.RuneEntityUnimplemented;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.Vec3i;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
+
+import org.apache.logging.log4j.Level;
+
+import scala.util.Random;
+import xilef11.mc.runesofwizardry_classics.ModLogger;
+import xilef11.mc.runesofwizardry_classics.Refs;
+import xilef11.mc.runesofwizardry_classics.runes.entity.RuneEntityResurrection;
 
 import com.zpig333.runesofwizardry.api.RuneEntity;
 import com.zpig333.runesofwizardry.core.rune.PatternUtils;
 import com.zpig333.runesofwizardry.tileentity.TileEntityDustActive;
 
 public class RuneResurrection extends ClassicRune {
-
+	
+	public static Map<String,List<String>> dropToEntity=null;
+	public RuneResurrection() {
+		super();
+		MinecraftForge.EVENT_BUS.register(this);
+	}
 	@Override
 	protected ItemStack[][] setupPattern() throws IOException {
 		return PatternUtils.importFromJson(Refs.PATTERN_PATH+"runeResurrection.json");
@@ -55,7 +83,70 @@ public class RuneResurrection extends ClassicRune {
 	@Override
 	public RuneEntity createRune(ItemStack[][] actualPattern, EnumFacing front,
 			Set<BlockPos> dusts, TileEntityDustActive entity) {
-		return new RuneEntityUnimplemented(actualPattern, front, dusts, entity, this);
+		return new RuneEntityResurrection(actualPattern, front, dusts, entity, this);
+	}
+	//not guaranteed to work with modded entities though
+	@SubscribeEvent
+	public void initDropsTable(WorldEvent.Load event){
+		//if(dropToEntity!=null)return;
+		dropToEntity = new HashMap<String,List<String>>();
+		for(String entName:EntityList.getEntityNameList()){
+			Entity e = EntityList.createEntityByName(entName, MinecraftServer.getServer().getEntityWorld());
+			if(e instanceof EntityLiving){//if its a mob
+				EntityLiving ent = (EntityLiving)e;
+				e.captureDrops=true;
+				Method getdrops = ReflectionHelper.findMethod(EntityLivingBase.class, (EntityLivingBase)ent, new String[]{"dropFewItems","func_70628_a"},boolean.class,int.class);
+				try {
+					getdrops.invoke(ent,true, 10);
+				} catch (IllegalAccessException e1) {
+					ModLogger.logException(Level.ERROR, e1, "Exception when trying to get drops from entity: "+ent);
+					continue;
+				} catch (IllegalArgumentException e1) {
+					ModLogger.logException(Level.ERROR, e1, "Exception when trying to get drops from entity: "+ent);
+					continue;
+				} catch (InvocationTargetException e1) {
+					ModLogger.logException(Level.ERROR, e1, "Exception when trying to get drops from entity: "+ent);
+					continue;
+				}
+				for(EntityItem item:ent.capturedDrops){
+					ItemStack stack = item.getEntityItem();
+					String key = stack.getItem().getRegistryName()+stack.getMetadata();
+					List<String> ids = dropToEntity.get(key);
+					if(ids==null){
+						ids=new LinkedList<String>();
+						dropToEntity.put(key, ids);
+					}
+					ids.add(entName);
+				}
+				ent.setDead();
+			}
+		}
+	}
+
+	/**
+	 * Returns the ID of a random entity that might drop all items passed in (ignores NBT)
+	 * @param drops the stacks for which to get a random entity
+	 * @return a random entity that may drop the items passed, or null if there is none.
+	 */
+	public String entityIDFromDrops(Collection<ItemStack> drops){
+		List<String> possible = null;
+		for(ItemStack s:drops){
+			String key = s.getItem().getRegistryName()+s.getMetadata();
+			List<String> entities = dropToEntity.get(key);
+			if(entities==null)return null;
+			if(possible==null){
+				possible = new LinkedList<String>();
+				possible.addAll(entities);
+			}else{
+				Iterator<String> it = possible.iterator();
+				while(it.hasNext()){
+					if(!entities.contains(it.next()))it.remove();
+				}
+			}
+		}
+		if(possible.size()==0)return null;
+		Random rand = new Random();
+		return possible.get(rand.nextInt(possible.size()));
 	}
 
 }
---
+++ src/main/java/xilef11/mc/runesofwizardry_classics/runes/entity/RuneEntityResurrection.java
@@ -0,0 +1,70 @@
+package xilef11.mc.runesofwizardry_classics.runes.entity;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import xilef11.mc.runesofwizardry_classics.runes.RuneResurrection;
+
+import com.zpig333.runesofwizardry.api.RuneEntity;
+import com.zpig333.runesofwizardry.tileentity.TileEntityDustActive;
+
+public class RuneEntityResurrection extends RuneEntity {
+	public RuneEntityResurrection(ItemStack[][] actualPattern,
+			EnumFacing facing, Set<BlockPos> dusts,
+			TileEntityDustActive entity, RuneResurrection creator) {
+		super(actualPattern, facing, dusts, entity, creator);
+	}
+	@Override
+	public void onRuneActivatedbyPlayer(EntityPlayer player,
+			ItemStack[] sacrifice, boolean negated) {
+		if(!player.worldObj.isRemote){
+			//nothing I think
+		}
+	}
+	private static final int RAD=2;
+	@Override
+	public void update() {
+		World world = entity.getWorld();
+		if(!world.isRemote){
+			List<EntityItem> items = world.getEntitiesWithinAABB(EntityItem.class, new AxisAlignedBB(getPos().add(-RAD,0,-RAD), getPos().add(RAD,1,RAD)));
+			List<ItemStack> stacks = new LinkedList<ItemStack>();
+			int numItems = 0;
+			for(EntityItem ei:items){
+				ItemStack s = ei.getEntityItem();
+				stacks.add(s);
+				numItems+=s.stackSize;
+			}
+			if(numItems>1){
+				//try to get the entity
+				String id = ((RuneResurrection)creator).entityIDFromDrops(stacks);
+				//spawn the entity
+				if(id!=null){
+					for(EntityItem ei:items){
+						ei.setDead();
+						((WorldServer)world).spawnParticle(EnumParticleTypes.SMOKE_NORMAL, false, ei.posX, ei.posY, ei.posZ, 1, 0d, 0.5d, 0d, 0d);
+					}
+					Entity e = EntityList.createEntityByName(id, world);
+					e.setPositionAndUpdate(getPos().getX()+0.5, getPos().getY()+0.5, getPos().getZ()+0.5);
+					//XXX could add a delay before spawning the entity
+					world.spawnEntityInWorld(e);
+					world.playSoundAtEntity(e, "mob.chicken.plop", 0.5F, 0.8F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
+					this.onPatternBroken();
+				}
+			}
+		}
+
+	}
+
+}
