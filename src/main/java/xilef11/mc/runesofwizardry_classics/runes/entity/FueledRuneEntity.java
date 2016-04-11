e669513efce9b9ffb50765bb2fd11ea7858f4be6
Xilef11 <Xilef11@users.noreply.github.com>
2016-04-11 14:19:28 -0400

    added rune of power distribution

--- src/main/java/xilef11/mc/runesofwizardry_classics/runes/entity/FueledRuneEntity.java
+++ src/main/java/xilef11/mc/runesofwizardry_classics/runes/entity/FueledRuneEntity.java
@@ -3,6 +3,9 @@
  */
 package xilef11.mc.runesofwizardry_classics.runes.entity;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import net.minecraft.block.state.IBlockState;
@@ -11,6 +14,7 @@ import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
@@ -20,6 +24,7 @@ import net.minecraft.world.World;
 import com.zpig333.runesofwizardry.api.IRune;
 import com.zpig333.runesofwizardry.api.RuneEntity;
 import com.zpig333.runesofwizardry.tileentity.TileEntityDustActive;
+import com.zpig333.runesofwizardry.tileentity.TileEntityDustPlaced;
 
 /** This class is for runes that use fuel
  * @author Xilef11
@@ -27,6 +32,7 @@ import com.zpig333.runesofwizardry.tileentity.TileEntityDustActive;
  */
 public abstract class FueledRuneEntity extends RuneEntity {
 	protected int ticksLeft=0;
+	private List<RuneEntityPowerDistribution> poweredBy=new LinkedList<RuneEntityPowerDistribution>();
 	public FueledRuneEntity(ItemStack[][] actualPattern, EnumFacing facing,
 			Set<BlockPos> dusts, TileEntityDustActive entity, IRune creator) {
 		super(actualPattern, facing, dusts, entity, creator);
@@ -41,6 +47,7 @@ public abstract class FueledRuneEntity extends RuneEntity {
 		if(!player.worldObj.isRemote){
 			this.ticksLeft=initialTicks();
 			//TODO register this rune to nearby runes of power distribution
+			findNearbyPowerRunes();
 		}
 	}
 	/** returns the number of ticks this rune lasts without fuel **/
@@ -49,14 +56,17 @@ public abstract class FueledRuneEntity extends RuneEntity {
 	protected void onFuelRunOut(){
 		this.onPatternBroken();
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see com.zpig333.runesofwizardry.api.RuneEntity#onPatternBroken()
 	 */
 	@Override
 	public void onPatternBroken() {
 		super.onPatternBroken();
-		//TODO unregister from nearby Runes of power distribution
+		//unregister from nearby Runes of power distribution
+		for(RuneEntityPowerDistribution p:poweredBy){
+			unregisterFrom(p);
+		}
 	}
 
 	/** Adds fuel to this rune. 1 life tick is added per fuel tick (by default) **/
@@ -74,25 +84,67 @@ public abstract class FueledRuneEntity extends RuneEntity {
 			if(ticksLeft<=0)this.onFuelRunOut();
 		}
 	}
-
+	private static final int FUEL_EAT_TICKS=3;
 	/* (non-Javadoc)
 	 * @see com.zpig333.runesofwizardry.api.RuneEntity#handleEntityCollision(net.minecraft.world.World, net.minecraft.util.BlockPos, net.minecraft.block.state.IBlockState, net.minecraft.entity.Entity)
 	 */
 	@Override
 	public boolean handleEntityCollision(World worldIn, BlockPos pos,
 			IBlockState state, Entity entityIn) {
-		if(entityIn instanceof EntityItem){
-			ItemStack stack = ((EntityItem)entityIn).getEntityItem();
-			//find the furnace burn time. if not hardcoded in vanilla furnace, it will check Forge's registered fuelHandlers
-			int burnTime = TileEntityFurnace.getItemBurnTime(stack);
-			if(burnTime!=0){
-				this.addFuel(burnTime*stack.stackSize);
-				worldIn.playSoundAtEntity(entityIn, "random.fizz", 1, 1);
-				worldIn.spawnParticle(EnumParticleTypes.SMOKE_LARGE, entityIn.posX, entityIn.posY, entityIn.posZ, 0, 0, 0);
-				entityIn.setDead();
+		if(entity.ticksExisted()%FUEL_EAT_TICKS==0){
+			if(entityIn instanceof EntityItem){
+				ItemStack stack = ((EntityItem)entityIn).getEntityItem();
+				//find the furnace burn time. if not hardcoded in vanilla furnace, it will check Forge's registered fuelHandlers
+				int burnTime = TileEntityFurnace.getItemBurnTime(stack);
+				if(burnTime!=0){
+					this.addFuel(burnTime*stack.stackSize);
+					worldIn.playSoundAtEntity(entityIn, "random.fizz", 1, 1);
+					worldIn.spawnParticle(EnumParticleTypes.SMOKE_LARGE, entityIn.posX, entityIn.posY, entityIn.posZ, 0, 0, 0);
+					entityIn.setDead();
+				}
 			}
 		}
 		return true;
+	}
+	public void registerTo(RuneEntityPowerDistribution power){
+		if(!poweredBy.contains(power)){
+			poweredBy.add(power);
+			power.register(this);
+		}
+	}
+	public void unregisterFrom(RuneEntityPowerDistribution power){
+		poweredBy.remove(power);
+		power.unregister(this);
+	}
+	private void findNearbyPowerRunes(){
+		final int range =RuneEntityPowerDistribution.RANGE,
+				vrange = RuneEntityPowerDistribution.VRANGE;
+		World world = entity.getWorld();
+		if(!world.isRemote){
+			BlockPos thisPos = getPos();
+			Set<BlockPos> checked = new HashSet<BlockPos>();
+			checked.addAll(dustPositions);
+			for(int dx=-range;dx<=range;dx++){
+				for(int dz=-range;dz<=range;dz++){
+					for(int dy=-vrange;dy<=vrange;dy++){
+						BlockPos current = thisPos.add(dx,dy,dz);
+						//don't check if we already found that rune
+						if(checked.contains(current))continue;
+						TileEntity t = world.getTileEntity(current);
+						//don't do anything with non dust blocks
+						if(!(t instanceof TileEntityDustPlaced))continue;
+						TileEntityDustPlaced ted = (TileEntityDustPlaced)t;
+						//get the rune
+						RuneEntity rune = ted.getRune();
+						if(rune!=null)checked.addAll(rune.dustPositions);
+						//only find runes of power distribution
+						if(!(rune instanceof RuneEntityPowerDistribution))continue;
+						//add the rune to our list
+						registerTo((RuneEntityPowerDistribution) rune);
+					}
+				}
+			}
+		}
 	}
 	/* (non-Javadoc)
 	 * @see com.zpig333.runesofwizardry.api.RuneEntity#readFromNBT(net.minecraft.nbt.NBTTagCompound)
@@ -111,5 +163,5 @@ public abstract class FueledRuneEntity extends RuneEntity {
 		super.writeToNBT(compound);
 		compound.setInteger("FueledRune:ticksLeft", ticksLeft);
 	}
-	
+
 }
---
+++ src/main/java/xilef11/mc/runesofwizardry_classics/runes/entity/RuneEntityPowerDistribution.java
@@ -0,0 +1,203 @@
+package xilef11.mc.runesofwizardry_classics.runes.entity;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagIntArray;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
+import xilef11.mc.runesofwizardry_classics.ModLogger;
+import xilef11.mc.runesofwizardry_classics.Refs;
+
+import com.zpig333.runesofwizardry.api.IRune;
+import com.zpig333.runesofwizardry.api.RuneEntity;
+import com.zpig333.runesofwizardry.tileentity.TileEntityDustActive;
+import com.zpig333.runesofwizardry.tileentity.TileEntityDustPlaced;
+
+public class RuneEntityPowerDistribution extends FueledRuneEntity {
+
+	public RuneEntityPowerDistribution(ItemStack[][] actualPattern,
+			EnumFacing facing, Set<BlockPos> dusts,
+			TileEntityDustActive entity, IRune creator) {
+		super(actualPattern, facing, dusts, entity, creator);
+	}
+
+	@Override
+	protected int initialTicks() {
+		return Refs.TPS;//not really used, but must be >0
+	}
+
+	/* (non-Javadoc)
+	 * @see xilef11.mc.runesofwizardry_classics.runes.entity.FueledRuneEntity#onFuelRunOut()
+	 */
+	@Override
+	protected void onFuelRunOut() {
+		//not really used
+		this.ticksLeft=0;//make sure it never goes under 0
+	}
+
+	/* (non-Javadoc)
+	 * @see xilef11.mc.runesofwizardry_classics.runes.entity.FueledRuneEntity#onRuneActivatedbyPlayer(net.minecraft.entity.player.EntityPlayer, net.minecraft.item.ItemStack[], boolean)
+	 */
+	@Override
+	public void onRuneActivatedbyPlayer(EntityPlayer player,
+			ItemStack[] sacrifice, boolean negated) {
+		//super.onRuneActivatedbyPlayer(player, sacrifice, negated); don't register to other runes
+		World world = player.worldObj;
+		if(!world.isRemote){
+			findNearbyRunes();
+			initialised=true;
+		}
+	}
+	//the runes that are being powered by this one
+	private List<FueledRuneEntity> poweredRunes=new LinkedList<FueledRuneEntity>();
+	private static final int TICKRATE=10;
+	/* (non-Javadoc)
+	 * @see xilef11.mc.runesofwizardry_classics.runes.entity.FueledRuneEntity#update()
+	 */
+	@Override
+	public void update() {
+		if(!initialised)init();
+		//super.update(); - don't consume fuel
+		if(!entity.getWorld().isRemote){
+			if(entity.ticksExisted()%TICKRATE==0){
+				//add fuel to the runes
+				for(FueledRuneEntity fe:poweredRunes){
+					if(this.ticksLeft>TICKRATE){
+						fe.addFuel(TICKRATE);
+						ticksLeft-=TICKRATE;
+					}
+				}
+			}
+		}
+	}
+	/**Horizontal radius of the powering**/
+	public static final int RANGE=32;
+	/**Vertical range **/
+	public static final int VRANGE=8;
+	private void findNearbyRunes(){
+		World world = entity.getWorld();
+		if(!world.isRemote){
+			poweredRunes = new LinkedList<FueledRuneEntity>();
+			Set<BlockPos> checkedPositions = new HashSet<BlockPos>();
+			BlockPos thisPos = getPos();
+			//don't check this rune
+			checkedPositions.addAll(this.dustPositions);
+			for(int dx=-RANGE;dx<=RANGE;dx++){
+				for(int dz=-RANGE;dz<=RANGE;dz++){
+					for(int dy=-VRANGE;dy<=VRANGE;dy++){
+						BlockPos current = thisPos.add(dx,dy,dz);
+						//don't check if we already found that rune
+						if(checkedPositions.contains(current))continue;
+						TileEntity t = world.getTileEntity(current);
+						//don't do anything with non dust blocks
+						if(!(t instanceof TileEntityDustPlaced))continue;
+						TileEntityDustPlaced ted = (TileEntityDustPlaced)t;
+						//get the rune
+						RuneEntity rune = ted.getRune();
+						if(rune!=null)checkedPositions.addAll(rune.dustPositions);
+						//skip non-fuelable runes
+						if(!(rune instanceof FueledRuneEntity))continue;
+						//don't affect runes of power distribution
+						if(rune instanceof RuneEntityPowerDistribution)continue;
+						//add the rune to our list
+						//poweredRunes.add((FueledRuneEntity)rune);
+						((FueledRuneEntity)rune).registerTo(this);
+					}
+				}
+			}
+		}
+	}
+	/**Adds a rune to be powered by this**/
+	public void register(FueledRuneEntity toPower){
+		poweredRunes.add(toPower);
+	}
+	/**removes a rune to be powered by this**/
+	public void unregister(FueledRuneEntity toPower){
+		poweredRunes.remove(toPower);
+	}
+	private Set<BlockPos> toInit=null;
+	private boolean initialised=false;
+	private void init(){
+		if(initialised || toInit==null ||entity==null ||entity.getWorld()==null)return;
+		for(BlockPos p:toInit){
+			TileEntity t = entity.getWorld().getTileEntity(p);
+			if(t instanceof TileEntityDustPlaced){
+				TileEntityDustPlaced te = (TileEntityDustPlaced)t;
+				RuneEntity rune = te.getRune();
+				if((rune instanceof FueledRuneEntity) && !(rune instanceof RuneEntityPowerDistribution)){
+					((FueledRuneEntity)rune).registerTo(this);;
+				}else{
+					ModLogger.logError("rune at "+p+" was not appropriate for power distribution");
+				}
+			}else{
+				ModLogger.logError("TileEntity at "+p+" was not placed dust for power distribution");
+			}
+		}
+		initialised=true;
+	}
+	/* (non-Javadoc)
+	 * @see xilef11.mc.runesofwizardry_classics.runes.entity.FueledRuneEntity#readFromNBT(net.minecraft.nbt.NBTTagCompound)
+	 */
+	@Override
+	public void readFromNBT(NBTTagCompound compound) {
+		super.readFromNBT(compound);
+		NBTTagList runes = (NBTTagList) compound.getTag("PoweredRunes");
+		toInit = new HashSet<BlockPos>();
+		for(int i=0;i<runes.tagCount();i++){
+			int[] c = runes.getIntArrayAt(i);
+			BlockPos pos = new BlockPos(c[0],c[1],c[2]);
+			toInit.add(pos);//we will need to initialise later on an update tick because world may be null on first load
+		}
+		initialised=false;
+	}
+
+	/* (non-Javadoc)
+	 * @see xilef11.mc.runesofwizardry_classics.runes.entity.FueledRuneEntity#writeToNBT(net.minecraft.nbt.NBTTagCompound)
+	 */
+	@Override
+	public void writeToNBT(NBTTagCompound compound) {
+		super.writeToNBT(compound);
+		NBTTagList runes = new NBTTagList();
+		for(FueledRuneEntity ent:poweredRunes){
+			BlockPos pos = ent.getPos();
+			NBTTagIntArray coords = new NBTTagIntArray(new int[]{pos.getX(),pos.getY(),pos.getZ()});
+			runes.appendTag(coords);
+		}
+		compound.setTag("PoweredRunes", runes);
+	}
+
+	/* (non-Javadoc)
+	 * @see xilef11.mc.runesofwizardry_classics.runes.entity.FueledRuneEntity#registerTo(xilef11.mc.runesofwizardry_classics.runes.entity.RuneEntityPowerDistribution)
+	 */
+	@Override
+	public void registerTo(RuneEntityPowerDistribution power) {
+		// NOTHING
+		ModLogger.logWarn("Something tried to power a rune of power distribution...");
+	}
+	@Override
+	public void unregisterFrom(RuneEntityPowerDistribution power) {
+		// NOTHING
+		ModLogger.logWarn("Something tried to unregister a rune of power distribution...");
+	}
+	/* (non-Javadoc)
+	 * @see xilef11.mc.runesofwizardry_classics.runes.entity.FueledRuneEntity#onPatternBroken()
+	 */
+	@Override
+	public void onPatternBroken() {
+		super.onPatternBroken();
+		for(FueledRuneEntity f:poweredRunes){
+			f.unregisterFrom(this);
+		}
+	}
+
+	
+}
