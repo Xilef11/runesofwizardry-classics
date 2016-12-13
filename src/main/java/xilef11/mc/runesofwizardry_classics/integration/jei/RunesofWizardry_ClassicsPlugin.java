f83d3cc9b031a69fedb4bfc6b15b54d3669288b6
Xilef11 <Xilef11@users.noreply.github.com>
2016-12-13 13:36:57 -0500

    updated Forge and JEI

--- src/main/java/xilef11/mc/runesofwizardry_classics/integration/jei/RunesofWizardry_ClassicsPlugin.java
+++ src/main/java/xilef11/mc/runesofwizardry_classics/integration/jei/RunesofWizardry_ClassicsPlugin.java
@@ -3,7 +3,9 @@ package xilef11.mc.runesofwizardry_classics.integration.jei;
 import mezz.jei.api.IJeiRuntime;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
+import mezz.jei.api.ISubtypeRegistry;
 import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.ingredients.IModIngredientRegistration;
 import net.minecraft.item.ItemStack;
 import xilef11.mc.runesofwizardry_classics.Refs;
 import xilef11.mc.runesofwizardry_classics.items.ItemSpiritPickaxe;
@@ -20,6 +22,16 @@ public class RunesofWizardry_ClassicsPlugin implements IModPlugin{
 
 	@Override
 	public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
+		
+	}
+
+	@Override
+	public void registerItemSubtypes(ISubtypeRegistry subtypeRegistry) {
+		
+	}
+
+	@Override
+	public void registerIngredients(IModIngredientRegistration registry) {
 		
 	}
 
