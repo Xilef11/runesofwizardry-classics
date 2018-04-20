6b5f4af461d1ba8f435bf979b3655172ae76598d
Xilef11 <Xilef11@users.noreply.github.com>
2018-04-20 12:27:30 -0400

    update libraries

--- src/main/java/xilef11/mc/runesofwizardry_classics/integration/guideapi/GuideBookAdditions.java
+++ src/main/java/xilef11/mc/runesofwizardry_classics/integration/guideapi/GuideBookAdditions.java
@@ -4,6 +4,7 @@ import com.zpig333.runesofwizardry.integration.guideapi.WizardryGuide;
 import com.zpig333.runesofwizardry.integration.guideapi.category.CategoryDusts;
 import com.zpig333.runesofwizardry.integration.guideapi.category.CategoryRunes;
 
+import amerifrance.guideapi.api.GuideAPI;
 import amerifrance.guideapi.api.impl.Book;
 import amerifrance.guideapi.api.impl.abstraction.CategoryAbstract;
 import amerifrance.guideapi.api.impl.abstraction.EntryAbstract;
@@ -18,7 +19,7 @@ import xilef11.mc.runesofwizardry_classics.items.ItemSpiritSword;
 public class GuideBookAdditions {
 	/**Add a few things to the entries created automatically by the base mod **/
 	public static void handlePost(){
-		Book book = WizardryGuide.BOOK;
+		Book book = GuideAPI.getBooks().get(WizardryGuide.LOCATION);
 		//placeholder
 		CategoryAbstract dusts = BookHelper.getCategoryFromName(book, WizardryGuide.CAT_LOC+CategoryDusts.NAME);
 		EntryAbstract placeholders = BookHelper.getEntryFromName(dusts, CategoryDusts.ENTRY_KEY+"placeholders");
